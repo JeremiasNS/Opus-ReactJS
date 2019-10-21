5f08f68d358e6618a7dfa5f99cfab4b6736b2839
Jeremias Neres <jeremiasneres23@gmail.com>
2019-10-21 17:45:11 -0300

    Adicionando passagem de parametros via Callbacks functions

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import './Menu.css'
 import CadEmpresa from './CadEmpresa';
@@ -6,26 +6,57 @@ import Main from './main';
 
 
 
- export default function Menu() {
+ class Menu extends Component{
+
+     constructor(props){
+         super(props)
+         this.titleWasClicked = this.titleWasClicked.bind(this)
+         
+         this.state={
+             num: ''
+         };
+     }
+
+     titleWasClicked (event){
+
+        let num = "a";
+        
+         event.preventDefault()
+         const {dataCallbalck} = this.props
+         
+         if(num == "b"){
+             dataCallbalck('deu certo')
+        }else{
+            dataCallbalck('deu errado')
+        }
+
+
+
+         
+     }
+    
+    render(){
+
+
     return(
         <div className="Menu">
             <nav>
                 <ul>
                     <li><a>Candidato</a>
                         <ul className="dropdown1">
-                            <li><a >Cadastrar Candidato</a></li>
+                            <li><a onClick={this.titleWasClicked} >Cadastrar Candidato</a></li>
                             <li><a>Consultas do Candidato</a></li>
                         </ul>
                     </li>
                     <li><a>Empresa</a>
                         <ul className="dropdown1">
-                            <li><a href="#" handleClick={Main.ok}>Cadastrar Empresa</a></li>
+                            <li><a onClick={this.titleWasClicked}>Cadastrar Empresa</a></li>
                             <li><a>Consultas da Empresa</a></li>
                         </ul>
                     </li>
                     <li><a>Vagas</a>
                         <ul className="dropdown1">
-                            <li><a>Cadastrar Vagas</a></li>
+                            <li><a onClick={this.titleWasClicked} id="b">Cadastrar Vagas</a></li>
                             <li><a>Consultas de Vagas</a></li>
                         </ul>
                     </li>
@@ -34,3 +65,5 @@ import Main from './main';
         </div>
     );
 }
+} 
+export default Menu;
--- src/components/main.js
+++ src/components/main.js
@@ -3,16 +3,22 @@ import Menu from './Menu';
 import Footer from './Footer';
 
 
+
 import './main.css';
 
 
 class Main extends Component{
 
+  handleDataCallback(txtMsg) {
+    alert(txtMsg)
+    console.log(this)
+  }
+
   render (){
       return(
       <div className="Main">
         <header></header>
-          <Menu/>
+         <Menu dataCallbalck={this.handleDataCallback}/>
         <main>
           <div className="left"></div>
           <div className="middle">        
